ffb4e17781d49158e84b9bec98cb14c9665ad4eb
Sifiso Kubheka <Sifiso Kubheka@DESKTOP-HQ4AU1T>
2024-10-30 04:05:25 +0200

    changes!

--- src/main/java/com/mycompany/progpart2/PROGPART2.java
+++ src/main/java/com/mycompany/progpart2/PROGPART2.java
@@ -3,7 +3,9 @@
  */
 
 package com.mycompany.progpart2;
-
+import java.util.Scanner;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 /**
  *
  * @author Sifiso Kubheka
@@ -11,6 +13,40 @@ package com.mycompany.progpart2;
 public class PROGPART2 {
 
     public static void main(String[] args) {
-        
+    //Passing the external classes
+    
+    
+    //Declarations
+    String firstname;
+    String lastname;
+    String username;
+    String password;
+    
+    //Creating object for scanner
+    Scanner input=new Scanner(System.in);
+    
+    //Creating object for JDiaolog
+    JDialog dialog = new JDialog();
+    dialog.setAlwaysOnTop(true);
+    
+    //Prompt the user to input their details
+    System.out.println("Enter First Name:");
+    firstname=input.next();
+    
+    System.out.println("Enter lastname:");
+    lastname = input.next();
+    
+    System.out.println("Enter username:");
+    username = input.next();
+    
+    System.out.println("Enter password:");
+    password = input.next();
+    
+    //registering the user
+    
+    
+    
+    
+    
     }
 }
--- src/main/java/com/mycompany/progpart2/login.java
+++ src/main/java/com/mycompany/progpart2/login.java
@@ -9,5 +9,51 @@ package com.mycompany.progpart2;
  * @author Sifiso Kubheka
  */
 public class login {
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String password;
     
+    //method to check the username
+    public boolean checkUsername(String username){
+    //check if the user name meets the conditions
+    return username.contains("_") && username.length()<=5;
+    }
+    
+    //method to check for password
+    public boolean checkPasswordComplexity(String password){
+    //check if the password is complex  
+    return password.length()>= 8 && password.matches(".*[A-Z].*") &&password.matches(".*[a-z].*") && password.matches(".*[0-9].*") && password.matches(".*[!@#$%^&().]*");
+
+    }
+    //method to register the user
+    public String registerUser(String firstname, String lastname, String username, String password){
+    if(!checkUsername(username)){
+        return "The username is incorrectly formatted.";
+        }
+    if(!checkPasswordComplexity(password)){
+        return "The password does not meet the complexity requirements.";
+        }
+       
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.username = username;
+    this.password = password;
+    return "conditions have been met and the user has been registered successfully.";
+    }
+     
+    //method to verify the login details
+    public boolean loginUser(String username, String password){
+    return username.equals(this.username) && password.equals(this.password);
+       
+    }
+   
+    public String returnLoginStatus(boolean successfulLogin){
+    if (successfulLogin){
+        return "Welcome "+ this.firstname +" "+ this.lastname+",it is great to see you again.";
+        }
+        else{
+            return "Username or password incorrect, try again.";
+        }
+    } 
 }
