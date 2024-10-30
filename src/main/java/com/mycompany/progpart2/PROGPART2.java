9cad64b56b1d249f03ac17222ce238e076e1ebaf
Sifiso Kubheka <Sifiso Kubheka@DESKTOP-HQ4AU1T>
2024-10-30 03:38:06 +0200

    Second Commit!

---
+++ src/main/java/com/mycompany/progpart2/PROGPART2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.progpart2;
+
+/**
+ *
+ * @author Sifiso Kubheka
+ */
+public class PROGPART2 {
+
+    public static void main(String[] args) {
+        
+    }
+}
