cce1c1003665742964f10c3adaa89013194208b9
Sifiso Kubheka <Sifiso Kubheka@DESKTOP-HQ4AU1T>
2024-10-30 14:35:02 +0200

    part 2

--- src/main/java/com/mycompany/progpart2/PROGPART2.java
+++ src/main/java/com/mycompany/progpart2/PROGPART2.java
@@ -14,8 +14,9 @@ public class PROGPART2 {
 
     public static void main(String[] args) {
     //Passing the external classes
-    
-    
+    login kby = new login();
+    task task = new task();
+  
     //Declarations
     String firstname;
     String lastname;
@@ -43,10 +44,51 @@ public class PROGPART2 {
     password = input.next();
     
     //registering the user
+    String registration= kby.registerUser(firstname,lastname,username,password);
+    System.out.println("Registration: "+ registration);
     
-    
-    
-    
-    
+    //login user
+        
+    System.out.println("Enter username to login:");
+    username=input.next();
+    System.out.println("Enter password to login:");
+    password=input.next();
+       
+    //dispaly  login Status
+    boolean loginSuccessful= kby.loginUser(username,password);
+    String loginMessage=kby.returnLoginStatus(loginSuccessful);
+    System.out.println(loginMessage);
+       
+    //IF statements 
+    if(loginSuccessful){
+    JOptionPane.showMessageDialog(dialog, "Welcome to EasyKanban");
+    while(loginSuccessful){
+    int choice = Integer.parseInt(JOptionPane.showInputDialog("""
+                                                              Enter choice: 
+                                                            1. Add Tasks
+                                                            2. Show report
+                                                            3. Quit"""));
+            
+    switch (choice) {
+    case 1:
+    //promt user to enter tasks
+    task.addTask();
+    break;
+    case 2:
+    // if the user selects option 2
+    JOptionPane.showMessageDialog(null, "coming soon");
+    break;
+    case 3:
+    //If the user selects option 3   
+    System.exit(0);
+    JOptionPane.showMessageDialog(null,"Exiting EasyKanban");
+    default:
+    break;
+    }
+    input.close();
+    dialog.dispose();
+    }
+       }
     }
 }
+
--- src/main/java/com/mycompany/progpart2/task.java
+++ src/main/java/com/mycompany/progpart2/task.java
@@ -3,11 +3,94 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.progpart2;
-
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 /**
  *
  * @author Sifiso Kubheka
  */
 public class task {
+    //declarations    
+    private String[] taskName;
+    private int[] taskNumber;
+    private String[] taskDescription;
+    private String[] developerDetails;
+    private int[] taskDuration;
+    private String[] taskID;
+    private String[] taskStatus;
     
-}
+    //Checking the task descriptions
+    public boolean checkTaskDescription(String taskDescription) {
+    return taskDescription.length() <= 50;
+    }
+    // creating the Task ID
+    public String createTaskID(String taskName, String developerDetails,int taskNumber) {
+    return taskName.substring(0, 2).toUpperCase() + ":" + taskNumber + ":" + developerDetails.substring(developerDetails.length() - 3).toUpperCase();
+    }
+
+    public String printTaskDetails(int details) {
+    // printing all the details stored
+    return "Task Status: " + taskStatus[details] + "\nDeveloper Details: " + developerDetails[details] + "\nTask Number: " + taskNumber[details] + "\nTask Name: " + taskName[details] + "\nTask Description: " + taskDescription[details] + "\nTask ID: " + taskID[details] + "\nDuration: " + taskDuration[details] + " hours";
+    }
+
+    //Return total hours
+    public int returnTotalHours(int taskDuration) {
+    return taskDuration;
+   
+    }
+    //Adding tasks
+    public void addTask() {
+    String [] options = new String []{"To Do", "Done", "Doing"};    
+    String input = JOptionPane.showInputDialog("How many tasks do you wish to enter");
+    // asking for the number of tasks user wants to input
+    int numTasks = 0;
+    try {
+        numTasks = Integer.parseInt(input);
+    
+    for (int count = 0; count < numTasks; count++) {
+            
+    taskName = new String[numTasks];
+    taskName[count] = JOptionPane.showInputDialog("Enter Task Name " + (count + 1) + ":");
+        
+    taskNumber = new int[numTasks];
+    taskNumber[count] = count;
+        
+    taskDescription = new String [numTasks];
+    do{
+    taskDescription[count] = JOptionPane.showInputDialog("Enter Task Description:");  
+          
+            
+    // checking whether the task description meets the minimum requirements
+    if(!checkTaskDescription(taskDescription[count])){
+    JOptionPane.showMessageDialog(null, "Task description is too long.Please try again");
+    count--;
+    continue;
+            }
+        }
+    while (taskDescription[count].length()>50);
+        
+    developerDetails = new String [numTasks];
+    developerDetails[count] = JOptionPane.showInputDialog("Enter Developer Details:");
+        
+    taskDuration = new int [numTasks];
+    taskDuration[count]  = Integer.parseInt( JOptionPane.showInputDialog("Enter Task Duration (in hours):"));
+       
+    taskID = new String [numTasks];
+    taskID[count] = createTaskID(taskName[count],developerDetails[count],count);
+    JOptionPane.showMessageDialog(null,"taskID"+ taskID[count]);
+        
+    taskStatus = new String[numTasks];
+    taskStatus [count] = (String)JOptionPane.showInputDialog(null, "taskStatus: ","choose your status ",JOptionPane.QUESTION_MESSAGE,null,options,options[2]);
+        
+    // Printing the details of the captured tasks
+    JOptionPane.showMessageDialog(null,"Tasks successfully captured:" + printTaskDetails(count)); 
+    }
+
+    }catch(NumberFormatException e){
+    JOptionPane.showMessageDialog(null, "Try again");
+    return;
+    }
+
+ }
+ }
+
